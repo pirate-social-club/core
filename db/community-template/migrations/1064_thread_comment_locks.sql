e4aa914d5d1955ca8df092389230cf349763cdb7
Pirate Dev <dev@pirate.local>
2026-05-05 18:10:18 +0400

    Add community lock and link language schema updates

---
+++ db/community-template/migrations/1064_thread_comment_locks.sql
@@ -0,0 +1,25 @@
+ALTER TABLE posts
+  ADD COLUMN comments_locked INTEGER NOT NULL DEFAULT 0
+  CHECK (comments_locked IN (0, 1));
+
+ALTER TABLE posts
+  ADD COLUMN comments_locked_at TEXT;
+
+ALTER TABLE posts
+  ADD COLUMN comments_locked_by_user_id TEXT;
+
+ALTER TABLE posts
+  ADD COLUMN comments_lock_reason TEXT;
+
+ALTER TABLE comments
+  ADD COLUMN replies_locked INTEGER NOT NULL DEFAULT 0
+  CHECK (replies_locked IN (0, 1));
+
+ALTER TABLE comments
+  ADD COLUMN replies_locked_at TEXT;
+
+ALTER TABLE comments
+  ADD COLUMN replies_locked_by_user_id TEXT;
+
+ALTER TABLE comments
+  ADD COLUMN replies_lock_reason TEXT;
---
+++ db/control-plane/migrations/0080_control_plane_link_enrichment_source_language.sql
@@ -0,0 +1,2 @@
+ALTER TABLE link_enrichments
+    ADD COLUMN source_language TEXT;
